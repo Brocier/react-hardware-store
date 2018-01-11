8deb8ea6264909aff75947fdf02ccc7fdc2c466e
Brocier <jschoe04@gmail.com>
2018-01-11 18:03:54 -0500

    reverted changes back to code-along and added code from solutions where the turnary was messing me up big time

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 // src/components/Product.js
 
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 class Product extends Component {
   deleteProduct = () => {
@@ -8,20 +8,63 @@ class Product extends Component {
       .props
       .deleteProductFromProductList(this.props.index)
   }
+  addToCart = () => {
+    this
+      .props
+      .addProductToCart(this.props.index)
+  }
+
+  removeFromCart = () => {
+    this
+      .props
+      .removeProductFromCart(this.props.index)
+  }
+
+  showOptions = () => {
+    const viewMode = this.props.viewMode
+
+    const adminOptions = (
+      <div>
+        <button onClick={this.deleteProduct}>Delete</button>
+      </div>
+    )
+    const shopOptions = (
+      <div>
+        <button onClick={this.addToCart}>Add To Cart</button>
+      </div>
+    )
+    const cartOptions = (
+      <div>
+        <button onClick={this.removeFromCart}>Remove From Cart</button>
+      </div>
+    )
+
+    switch (viewMode) {
+      case 'ADMIN':
+        return adminOptions
+      case 'SHOP':
+        return shopOptions
+      case 'CART':
+        return cartOptions
+      default:
+        return null
+    }
+  }
+
   render() {
-    const productName = this.props.productName;
-    const description = this.props.description;
-    const price = this.props.price;
+    const productName = this.props.productName
+    const description = this.props.description
+    const price = this.props.price
 
     return (
       <div>
         <h3>{productName}</h3>
         <div>{description}</div>
         <div>${price}</div>
-        <button onClick={this.deleteProduct}>Delete {productName}</button>
+        {this.showOptions()}
       </div>
-    );
+    )
   }
 }
 
-export default Product;
+export default Product
